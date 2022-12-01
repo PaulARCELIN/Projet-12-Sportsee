2f233f222a2d488f37af0c7530a54bbc544afeca
Paul <paularcelin@hotmail.fr>
2022-12-01 15:14:15 +0100

    First commit

--- sportsee/src/App.js
+++ sportsee/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+/* fetch('http://localhost:3000/user/18')
+  .then(res => res.json())
+  .then(data => console.log(data.data)) */
+
+import Header from "./components/Header";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
